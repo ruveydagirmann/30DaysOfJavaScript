b6fa7750c44ba86c5bf1f23311a46938a37e062a
Ruveyda <agirmannruveyda@gmail.com>
2022-08-15 18:58:39 +0300

    day01

---
+++ day_01/variables.js
@@ -0,0 +1,28 @@
+console.log("30 days of javascript");
+
+let firstName = 'Ruveyda'  // first name of a person
+let country = 'Turkey'  // country
+let city = 'İstanbul' 
+let age = 22 
+let isMarried = false;
+
+console.log(firstName,country, city, age, isMarried) ;
+
+const gravity = 9.81;
+const boilingPoint = 100;
+const PI = 3.14 ;
+
+console.log(gravity, boilingPoint) ;
+
+
+let name = 'Ruveyda';
+let job = 'Engineer';
+let live = 'İstanbul';
+
+console.log(name, job, live);
+
+//Data types
+
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof isMarried);
